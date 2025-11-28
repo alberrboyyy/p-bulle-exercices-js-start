ad56b8c47609b1a6328e1ff1cef0d0a09a32aca4
Albert <87382668+alberrboyyy@users.noreply.github.com>
2025-11-28 13:51:06 +0100

    amusement-park

--- exercices/amusement-park/amusement-park.js
+++ exercices/amusement-park/amusement-park.js
@@ -10,7 +10,12 @@
  * @returns {Visitor} the visitor that was created
  */
 export function createVisitor(name, age, ticketId) {
-  throw new Error('Remove this line and implement the function');
+  const Visitor = {
+  name: name,
+  age: age,
+  ticketId: ticketId,
+}
+  return Visitor
 }
 
 /**
@@ -20,7 +25,8 @@ export function createVisitor(name, age, ticketId) {
  * @returns {Visitor} the visitor without a ticket
  */
 export function revokeTicket(visitor) {
-  throw new Error('Remove this line and implement the function');
+  visitor.ticketId = null
+  return visitor
 }
 
 /**
@@ -31,7 +37,13 @@ export function revokeTicket(visitor) {
  * @returns {string} ticket status
  */
 export function ticketStatus(tickets, ticketId) {
-  throw new Error('Remove this line and implement the function');
+  if (!(ticketId in tickets)) {
+    return "unknown ticket id"
+  }
+  if (tickets[ticketId] === null) {
+    return "not sold"
+  }
+  return `sold to ${tickets[ticketId]}`
 }
 
 /**
@@ -43,7 +55,10 @@ export function ticketStatus(tickets, ticketId) {
  * @returns {string} ticket status
  */
 export function simpleTicketStatus(tickets, ticketId) {
-  throw new Error('Remove this line and implement the function');
+  if (!(ticketId in tickets) || tickets[ticketId] === null) {
+    return "invalid ticket !!!"
+  }
+  return tickets[ticketId]
 }
 
 /**
@@ -53,5 +68,10 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error('Remove this line and implement the function');
+  try {
+    return visitor.gtc.version
+  }
+  catch {
+    return 
+  }
 }
