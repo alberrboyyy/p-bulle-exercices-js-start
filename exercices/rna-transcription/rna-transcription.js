0e37d20fa440c846871ff6aa440c3540d0449719
Albert Braimi <albertbraimi@gmail.com>
2026-01-09 08:39:39 +0100

    rna-transcription 30m

--- exercices/rna-transcription/rna-transcription.js
+++ exercices/rna-transcription/rna-transcription.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRna = () => {
-  throw new Error('Remove this line and implement the function');
+export const toRna = (dnaSeq) => {
+  let dna = dnaSeq.split("");
+  return dna.map(DnsToRna).join("");
 };
+
+function DnsToRna(dna) {
+  switch (dna) {
+    case "G":
+      return "C";
+    case "C":
+      return "G";
+    case "T":
+      return "A";
+    case "A":
+      return "U";
+    default:
+      return "";
+  }
+}
